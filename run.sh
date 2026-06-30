414d036bd2b7867e624fb88e9c4d81b37bf4e4c8
SamB11 <123003385+SamB11@users.noreply.github.com>
2026-06-30 14:06:13 +0100

    Add files via upload

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ echo "[$(date '+%y-%m-%d %H:%M:%S')] Verify unzip..."
 ls -lh $SCRATCH
 
 echo "[$(date '+%y-%m-%d %H:%M:%S')] Starting training..."
-python3 ./src/Data_Augmented_Training_hybrid.py --data $SCRATCH/dataset.csv --models $OUTPUTS_DIR
+python3 ./src/Data_Augmented_Training_hybrid.py --data $SCRATCH/data/dataset.csv --models $OUTPUTS_DIR
 
 echo "[$(date '+%y-%m-%d %H:%M:%S')] Copy results to home directory..."
 cd $SCRATCH
