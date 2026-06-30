e0ce7628458a5d89837827db7215e0eb2d75c89e
Rocyzas <pranas.rokas@gmail.com>
2026-06-30 18:47:53 +0100

    bash path fix

--- run_vit.sh
+++ run_vit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=hybrid_training
+#SBATCH --job-name=vit_training
 #SBATCH -p Teaching
 #SBATCH --gres=gpu:1
 
@@ -18,7 +18,7 @@ echo "[$(date '+%y-%m-%d %H:%M:%S')] Unzipping dataset to scratch..."
 unzip -q ./data.zip -d $SCRATCH/
 
 echo "[$(date '+%y-%m-%d %H:%M:%S')] Copying python file to scratch..."
-cp ./src/Data_Augmented_Training_hybrid.py $SCRATCH/Data_Augmented_Training_hybrid.py
+cp ./src/Data_Augmented_Training_vit.py $SCRATCH/Data_Augmented_Training_vit.py
 
 # I think this is the fix but I cant exactly remember the error msg
 echo "[$(date '+%y-%m-%d %H:%M:%S')] Copying source directory to scratch..."
@@ -28,9 +28,9 @@ echo "[$(date '+%y-%m-%d %H:%M:%S')] Verify setup..."
 ls -lh $SCRATCH
 
 echo "[$(date '+%y-%m-%d %H:%M:%S')] Starting training..."
-#python3 $SCRATCH/Data_Augmented_Training_hybrid.py --data $SCRATCH/data/dataset.csv --models $OUTPUTS_DIR
+
 cd $SCRATCH
-python3 Data_Augmented_Training_hybrid.py \
+python3 Data_Augmented_Training_vit.py \
     --data data/dataset.csv \
     --models outputs
     
